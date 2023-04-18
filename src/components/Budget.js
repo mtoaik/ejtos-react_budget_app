644e57b7f76b476b17e2c9cfa231e17963df853b
mtoaik <mtoaik.schwarz@gmail.com>
2023-04-18 16:51:57 -0400

    changes to budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch} = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({type:'SET_BUDGET', payload:newBudget});
+    };
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+         Budget: {/*<b>{currency}</b> */}
+        <input 
+            type="number"
+            step={10}
+            value={`${newBudget}`}
+            onChange={(e) => {
+                const upperLimit = 20000;
+                const updatedBudget = Number(e.target.value);
+                
+                if (updatedBudget <= upperLimit && updatedBudget >= totalExpenses) 
+                {
+                    setNewBudget(Number(updatedBudget));
+                    handleSubmit(e);
+                }
+                else if (updatedBudget < totalExpenses) {
+                    alert('You can not reduce the budget value lower than the spending');
+                }
+                else {
+                    alert(`Budget cannot exceed ${upperLimit}`) //${currency} 
+                }
+            }}
+        />
         </div>
     );
 };
